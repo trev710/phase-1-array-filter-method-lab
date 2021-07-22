4b6d41e311d666cc0623e125817818c7ec6d58b8
TrevorJohnson <trevor.assetsolutions@gmail.com>
2021-07-21 19:23:02 -0600

    commit tree c1e7d96c486d83a92e1fd1c862df5755dfc51a9b

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+function findMatching(arr,str){
+    let match = arr.filter(name=> str.toUpperCase() === name.toUpperCase());{
+        
+        return match// return match.toUpperCase()
+    }
+}
+function fuzzyMatch(arr,str){
+let fuzzy = arr.filter(name=> name.substring(0,str.length)=== str);
+    return fuzzy
+}
+function matchName(arr,str){
+    let gotIt = arr.filter(obj=> obj.name=== str);
+    return gotIt
+
+}
